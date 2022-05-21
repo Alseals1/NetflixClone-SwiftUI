36c585d0d394e741a26edb72dfb7641204168460
Alandis Seals <aseals@Alandiss-Ed-Farm-MacBook-Pro-2.local>
2022-05-21 08:17:45 -0500

    finished custom tabs

--- BuildNetflixClone/HelperViews/CustomTabSwitcher.swift
+++ BuildNetflixClone/HelperViews/CustomTabSwitcher.swift
@@ -9,6 +9,8 @@ enum CustomTabs: String {
 
 struct CustomTabSwitcher: View {
     
+    @State private var currentTab: CustomTabs = .episode
+    
     func widthForTab(_ tab: CustomTabs) -> CGFloat {
         let string = tab.rawValue
         return string.widthForString(usingFont: .systemFont(ofSize: 16, weight: .bold))
@@ -20,20 +22,25 @@ struct CustomTabSwitcher: View {
             // Custom tab picker
             
             ScrollView(.horizontal, showsIndicators: false) {
-                HStack {
+                HStack(spacing: 20) {
                     ForEach(tabs, id: \.self) { tab in
                         VStack {
                             //red bar
-                            Rectangle().fill(.red)
+                            Rectangle()
                                 .frame(width: widthForTab(tab), height: 6)
+                                .foregroundColor(tab == currentTab ? Color.red : Color.clear)
+                            
                             //buttons
                             Button(action: {
-                                //
+                                currentTab = tab
                             }, label: {
                                 Text(tab.rawValue)
                                     .font(.system(size: 16, weight: .bold))
+                                    .foregroundColor(tab == currentTab ? Color.white : Color.gray)
+
                             })
                             .buttonStyle(PlainButtonStyle())
+                            .frame(width: widthForTab(tab), height: 30)
                         }
         
                         
